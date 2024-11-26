e8770d41ec16e08cca9aff5abd2f57e90b268cda
RichardBrown1 <richardbrown1999@outlook.com>
2024-11-27 06:23:22 +1100

    shader building

--- shaders/compile.bat
+++
@@ -1,4 +0,0 @@
-C:/VulkanSDK/1.3.268.0/Bin/dxc.exe -T vs_6_0 -E VS_main -spirv -Fo v_shader.spv shader.hlsl
-C:/VulkanSDK/1.3.268.0/Bin/dxc.exe -T ps_6_0 -E FS_main -spirv -Fo f_shader.spv shader.hlsl
-
-pause
---
+++ shaders/compile.ps1
@@ -0,0 +1,16 @@
+# Define paths
+$shaderSourcePath = ".\"
+$compiledShadersPath = "..\out\build\x64-debug\shaders"
+$dxcPath = "C:\VulkanSDK\1.3.268.0\Bin\dxc.exe"
+
+# Create compiled shaders directory if it doesn't exist
+if (-Not (Test-Path -Path $compiledShadersPath)) {
+    New-Item -ItemType Directory -Path $compiledShadersPath
+}
+
+# Compile shaders
+Get-ChildItem -Path $shaderSourcePath -Filter *.hlsl | ForEach-Object {
+    $shortName = $_.Name -replace "\..*", "" 
+    & $dxcPath -T vs_6_0 -E VS_main -spirv -Fo $compiledShadersPath/v_$shortName.spv $_.FullName
+    & $dxcPath -T ps_6_0 -E FS_main -spirv -Fo $compiledShadersPath/f_$shortName.spv $_.FullName
+}
