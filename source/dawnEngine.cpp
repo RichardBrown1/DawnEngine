2cc03888357881ad0f8dacc65c9a6ba741f7a502
RichardBrown1 <richardbrown1999@outlook.com>
2025-01-25 08:38:39 +1100

    adjusted comments

--- source/dawnEngine.cpp
+++ source/dawnEngine.cpp
@@ -264,20 +264,15 @@ void::DawnEngine::addLightData(fastgltf::Asset& asset, glm::f32mat4x4& transform
 	if (!success) {
 		throw std::runtime_error("could not decompose matrix");
 	}
-	
 	quaterion = glm::normalize(quaterion);
 	
-//	glm::quat delta = glm::normalize(glm::quat(0.5f, 0.5f, 0.5f, 0.5f));
+// will this work on all angles? have to offset since blender 0,0,0 rotation has light pointing down;
 	const glm::quat delta = glm::normalize(glm::quat(0.499999940f, 0.5f, 0.499999940f, 0.5f));
 //	glm::quat target = glm::normalize(glm::quat(0.7f, 0.0f, 0.7f, 0.0f));
 //	glm::quat delta = glm::inverse(quaterion) * target;
 	quaterion = quaterion * delta;
 
 	l.rotation = glm::eulerAngles(quaterion);
-	//l.rotation.x = 0.0f;
-	//l.rotation.y = 3.14f;
-	//l.rotation.z = 0.0f;
-	///l.rotation.y = 1.5f;
 	memcpy(&l.color, &asset.lights[lightIndex].color, sizeof(glm::f32vec3));
 	l.type = static_cast<uint32_t>(asset.lights[lightIndex].type);
 	memcpy(&l.intensity, &asset.lights[lightIndex], sizeof(glm::f32) * 4);
