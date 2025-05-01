c9de0269cdc1c844c0eec9e097ef04aea1a75d53
RichardBrown1 <richardbrown1999@outlook.com>
2025-05-01 10:20:01 +1000

    removed comments

--- source/initialRender.cpp
+++ source/initialRender.cpp
@@ -8,22 +8,6 @@ namespace DawnEngine {
 			_device = device;
 	};
 
-//	InitialRender::InitialRender(wgpu::Device &device, const InitialRenderDescriptor* descriptor) {
-//		_device = descriptor->device;
-//		_queue = _device.GetQueue();
-//		
-//		_vertexShaderModule = Utilities::createShaderModule(
-//			_device,
-//			VERTEX_SHADER_LABEL,
-//			VERTEX_SHADER_PATH
-//		);
-//		_fragmentShaderModule = Utilities::createShaderModule(
-//			_device,
-//			FRAGMENT_SHADER_LABEL,
-//			FRAGMENT_SHADER_PATH
-//		);
-//	};
-
 	void InitialRender::generateGpuObjects(const GenerateGpuObjectsDescriptor* descriptor) {
 		createBindGroupLayout();
 		createPipeline();
