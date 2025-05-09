aeca2aa619027847a7c93bac65bca67894a52d5e
RichardBrown1 <richardbrown1999@outlook.com>
2025-05-10 00:37:09 +1000

    fixed variable naming and type

--- source/main.cpp
+++ source/main.cpp
@@ -7,15 +7,15 @@
 #include "host/host.hpp"
 
 namespace {
-	const std::string gltfDirectory = "models/cornellBox/"; //must end with "/"
-	const std::string gltfFileName = "cornellbox.gltf";
+	std::string gltfDirectory = "models/cornellBox/"; //must end with "/"
+	std::string gltfFileName = "cornellbox.gltf";
 }
 
 int main()
 {
 	try {
 		Engine dawnEngine = Engine();
-		auto up_asset = std::unique_ptr<fastgltf::Asset>(gltf::getAsset(gltfDirectory, gltfFilePath));
+		auto up_asset = std::unique_ptr<fastgltf::Asset>(gltf::getAsset(gltfDirectory, gltfFileName));
 		host::Objects objects = gltf::processAsset(
 			*up_asset.get(),
 			std::array<uint32_t, 2>{dawnEngine.screenDimensions.width, dawnEngine.screenDimensions.height});
