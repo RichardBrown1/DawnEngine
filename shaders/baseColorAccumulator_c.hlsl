a925a334796bb6347bcb437fa7f5698663067c2e
RichardBrown1 <richardbrown1999@outlook.com>
2025-05-30 09:28:29 +1000

    updated shader

--- shaders/baseColorAccumulator_c.hlsl
+++ shaders/baseColorAccumulator_c.hlsl
@@ -9,27 +9,28 @@ struct TextureMasterInfo //struct DawnEngine::InfoBufferLayout
 
 struct TextureInputInfo
 {
-    uint2 dimensions;
-    uint materialId;
+    uint stpId;
     uint PAD0;
+    uint PAD1;
+    uint PAD2;
 };
 
 RWTexture2D<float4> accumulatorTexture : register(u0, space0);
-StructuredBuffer<TextureMasterInfo> textureMasterBuffer : register(t1, space0);
-ConstantBuffer<TextureInputInfo> textureInputInfoBuffer : register(b2, space0);
-Texture2D inputTexture : register(t3, space0);
-SamplerState inputSamplerState : register(s4, space0);
+Texture2D<float2> texCoordTexture : register(t1, space0);
+Texture2D<uint> textureIdTexture : register(t2, space0);
+ConstantBuffer<TextureInputInfo> textureInputInfo : register(b0, space1);
+Texture2D inputTexture : register(t1, space1);
+SamplerState inputSamplerState : register(s2, space1);
 
 
 [numthreads(1,1,1)]
 void cs_main(uint2 dispatchThreadID: SV_DispatchThreadID)
 {
-    uint textureWidth, textureHeight; //accumulatorTexture and inputTexture are assumed to be of the same size
-    accumulatorTexture.GetDimensions(textureWidth, textureHeight);
-    TextureMasterInfo info = textureMasterBuffer.Load(textureWidth * dispatchThreadID.y + dispatchThreadID.x);
-    float4 inputTextureColor = inputTexture.Sample(inputSamplerState, info.texcoord);
-    if (info.materialId == textureInputInfoBuffer.materialId)
+    if (textureIdTexture[dispatchThreadID.xy] == textureInputInfo.stpId)
     {
-        accumulatorTexture[dispatchThreadID.xy] = inputTextureColor;
+        accumulatorTexture[dispatchThreadID.xy] = inputTexture.Sample(
+            inputSamplerState, 
+            texCoordTexture[dispatchThreadID.xy]
+        );
     }
 }
