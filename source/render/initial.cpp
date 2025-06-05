8f77284debe43a601409cd79fc4e608b90426fc8
RichardBrown1 <richardbrown1999@outlook.com>
2025-06-05 14:17:48 +1000

    change background to blue

--- source/render/initial.cpp
+++ source/render/initial.cpp
@@ -97,7 +97,7 @@ namespace render {
 				.view = baseColorTextureView,
 				.loadOp = wgpu::LoadOp::Clear,
 				.storeOp = wgpu::StoreOp::Store,
-				.clearValue = wgpu::Color{0.0f, 0.0f, 0.0f, 0.0f},
+				.clearValue = wgpu::Color{0.3f, 0.3f, 1.0f, 1.0f},
 			},
 			wgpu::RenderPassColorAttachment {
 				.view = normalTextureView,
